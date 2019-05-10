acdcac5a5f14ed4db9c8d73ae3eb8a33f971705f
Klint Kanopka <klint.kanopka@gmail.com>
2019-05-10 13:00:40 -0700

    first commit

---
+++ file_paths.R
@@ -0,0 +1,20 @@
+library(fs)
+
+dir_data <- here::here("data files")
+dir_output <- here::here("output")
+
+file_data_p1 <- path(dir_data, 
+                     "data for mini-project 1", 
+                     ext="xlsx")
+file_data_p2_4 <- path(dir_data, 
+                       "data for mini-projects 2-4, table 3.8 shavelson & Webb",
+                       ext="xlsx")
+file_data1_p5 <- path(dir_data, 
+                      "data 1 for mini-project 5", 
+                      ext="xlsx")
+file_data2_p5 <- path(dir_data, 
+                      "data 2 for mini-project 5", 
+                      ext="xlsx")
+file_data_p6 <- path(dir_data, 
+                     "problem 6 data", 
+                     ext="xlsx")
---
+++ main.R
@@ -0,0 +1,1 @@
+source("file_paths.R")
